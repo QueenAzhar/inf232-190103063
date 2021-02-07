faec547206f15db924eaa71784aa1cd065aefd7b
QueenAzhar <190103063@stu.sdu.edu.kz>
2021-02-07 20:32:12 +0300

    renamed and included changes on file

--- Example.php
+++ Example.php
@@ -19,4 +19,20 @@ $cars=array
 
 echo "Normal count: " . count($cars)."<br>";
 echo "Recursive count: " . count($cars,1);
+
+
+echo date("l") . "<br>";
+
+
+echo date("l jS \of F Y h:i:s A") . "<br>";
+
+
+echo "Oct 3,1975 was on a ".date("l", mktime(0,0,0,10,3,1975)) . "<br>";
+
+
+echo date(DATE_RFC822) . "<br>";
+
+
+echo date(DATE_ATOM,mktime(0,0,0,10,3,1975));
+
 ?>
